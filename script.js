2fe3154c61d2328126d3961f041e5da893a178b8
Taynarsl <taynarsl11@hotmail.com>
2025-08-26 21:47:36 -0300

    incluiu validações de senha e email.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 document.getElementById ('formulario').addEventListener("submit", function(event) {
     event.preventDefault();
     let nome = document.getElementById('nome').value.trim();
+    let senha = document.getElementById('senha').value.trim();
+    let email = document.getElementById('email').value.trim();
     let valido = true;
+    let erros='';
     if(nome === ''){
         valido=false;
-
+        erros+='nome vazio, ';
+    }
+    if(!email.includes('@')){
+        valido=false;
+        erros+='email sem o arroba, ';
+    }
+    if(senha.length<6){
+        valido=false;
+        erros+='senha deve ter 6 caracteres ou mais';
     }
     if(valido){
         alert('Formulario enviado com sucesso.')
         this.reset()
+    }else{
+        alert('Formulario esta com erros: '+erros)   
     }
 
 });
